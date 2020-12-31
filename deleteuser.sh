475c724f65670b140dde3eb3757db3cee7ea7ad6
Olamide Olajide <34173234+olajio@users.noreply.github.com>
2020-12-31 05:37:19 -0600

    Create deleteuser.sh

---
+++ deleteuser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ 
+clear
+echo "Be aware that this script will remove the users and all of their data"
+sleep 2
+echo "If you would like to back up the users' data before deleting them, PRESS 'a' or 'A' to abort now. To continue deleting the user and the data, please y or Y"
+read response
+case $response in
+  a|A)
+    echo "Aborting..."
+    sleep 2
+    echo "Aborted";;
+  y|Y)
+    echo "Deleting users and their data..."
+    sleep 2
+    for name in $(cat names.csv)
+    do
+      if [ $(getent passwd $name) ]
+      then
+        userdel $name
+        rm -rf /home/$name
+        echo "User: $name and all its files and folders successfully deleted"
+        echo ""
+      else
+        echo "User: $name does NOT exist"
+      fi
+    done;;
+  *) echo "Invalid response - Bye...";;
+esac

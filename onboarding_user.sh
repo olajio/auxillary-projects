7a937bf2677541139081326161e15c7f9721aeb2
Olamide Olajide <34173234+olajio@users.noreply.github.com>
2021-01-08 23:45:15 -0600

    Update onboarding_user.sh

--- onboarding_user.sh
+++ onboarding_user.sh
@@ -1,28 +1,58 @@
 #!/bin/bash
  
 clear
-echo "Creating users..."
-sleep 2
-for name in $(cat names.csv)
-do
-  if [ $(getent passwd $name) ]
-  then
-    echo "User $name already exists"
-    echo ""
-    if [ ! -e /home/$name ]
+echo "To enter username from keyboard, enter '1'. To read names of users to be created from file, enter '2'"
+read response
+case $response in
+  1)
+    echo "Enter username"
+    read name
+    if [ $(getent passwd $name) ]
     then
-      mkdir /home/$name
-      mkdir /home/$name/.ssh
-      echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzKZyicHxIkklSrNlxsJyyTrcIdBIt84Z0cQb3R4k0jH53kxkaT5hP8tfWTe62LXi7vV86fY+SX7TBNM76XGCbw/6vrMGegm6J1x2i1AiLNwq5nqTjOGn0AIwku4IlCCLAB7tdfRyVuCarmBlwny3lzRyybIUAWXR/D6vpN09MsDILbKdhay+Q/p9OUBMSLPqXdY/QIh/Oe3rVv1lwY3AohNfq7V3tO88zKswfA5iiexNiSYX1myT0OrX8cBE771j9quoNZhQgaLI1mIMtAvnHQChrn9k2nUaO/BMBCQGol5XzGv1ado7hgoVPoluIUD+FGNo/pH4zcmDLICH6drXY/C9MESnkMUPLFxBXKO/OitApY71vRao9nAhAwpVMsy6FqiOb5uawhvhoHYIHTV/f4EtagVagRMP2PxYMYR6jykIV4MPJTkCm+lGhTyMlRu+qRQjdLn8AAtHf4aEV8dIkoGh088DI7eA/4o0wz4OV4upH5ewSFS+5IHmRECEW5Nc=" > /home/$name/.ssh/authorized_keys
-      echo "User: $name does NOT have a default home directory, but it has now been created for the user"
-      echo "Public ssh key has also been created for user: $name"
+      echo "User $name already exists"
       echo ""
-    fi
-  else
-    useradd -g developers -s /bin/bash -m -d /home/$name $name
-    mkdir /home/$name/.ssh
-    echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzKZyicHxIkklSrNlxsJyyTrcIdBIt84Z0cQb3R4k0jH53kxkaT5hP8tfWTe62LXi7vV86fY+SX7TBNM76XGCbw/6vrMGegm6J1x2i1AiLNwq5nqTjOGn0AIwku4IlCCLAB7tdfRyVuCarmBlwny3lzRyybIUAWXR/D6vpN09MsDILbKdhay+Q/p9OUBMSLPqXdY/QIh/Oe3rVv1lwY3AohNfq7V3tO88zKswfA5iiexNiSYX1myT0OrX8cBE771j9quoNZhQgaLI1mIMtAvnHQChrn9k2nUaO/BMBCQGol5XzGv1ado7hgoVPoluIUD+FGNo/pH4zcmDLICH6drXY/C9MESnkMUPLFxBXKO/OitApY71vRao9nAhAwpVMsy6FqiOb5uawhvhoHYIHTV/f4EtagVagRMP2PxYMYR6jykIV4MPJTkCm+lGhTyMlRu+qRQjdLn8AAtHf4aEV8dIkoGh088DI7eA/4o0wz4OV4upH5ewSFS+5IHmRECEW5Nc=" > /home/$name/.ssh/authorized_keys
-    echo "User $name successfully created"
-    echo ""
-  fi
-done
+      if [ ! -e /home/$name ]
+      then
+        mkdir /home/$name
+        mkdir /home/$name/.ssh
+        cp authorized_keys /home/$name/.ssh/authorized_keys
+        echo "User: $name does NOT have a default home directory, but it has now been created for the user"
+        echo "Public ssh key has also been created for user: $name"
+        echo ""
+      fi
+    else
+      useradd -g developers -s /bin/bash -m -d /home/$name $name
+      mkdir /home/$name/.ssh
+      cp authorized_keys /home/$name/.ssh/authorized_keys
+      echo "User $name successfully created"
+      echo ""
+    fi;;
+  2)
+    echo "Creating users..."
+    sleep 1
+    for name in $(cat names.csv)
+    do
+      if [ $(getent passwd $name) ]
+      then
+        echo "User $name already exists"
+        echo ""
+        if [ ! -e /home/$name ]
+        then
+          mkdir /home/$name
+          mkdir /home/$name/.ssh
+          cp authorized_keys /home/$name/.ssh/authorized_keys
+          echo "User: $name does NOT have a default home directory, but it has now been  created for the user"
+          echo "Public ssh key has also been created for user: $name"
+          echo ""
+        fi
+      else
+        useradd -g developers -s /bin/bash -m -d /home/$name $name
+        mkdir /home/$name/.ssh
+        cp authorized_keys /home/$name/.ssh/authorized_keys
+        echo "User $name successfully created"
+        echo ""
+      fi
+    done;;
+  *)
+    echo "Invalid entry... Bye!";;
+esac
